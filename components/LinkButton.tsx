d4ca7228d49dc04d7fcad6db35336b6f1c083204
ab-sc4t <ab.ayush2612@gmail.com>
2025-01-24 11:00:06 +0530

    on hover text-black and bg-gray-299

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -8,7 +8,7 @@ interface LinkButtonProps{
 export default function LinkButton(props: LinkButtonProps) {
     return (
         <Link
-            className="block rounded-md bg-black px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-36"
+            className="block rounded-md bg-black px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-200 hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-36"
             href={props.href}
         >
             {props.text}
