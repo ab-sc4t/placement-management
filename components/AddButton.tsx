6708c05c7f70272c8f8e555e5dffdb5454026a08
ab-sc4t <ab.ayush2612@gmail.com>
2025-01-24 11:00:42 +0530

    variantClass added and isSelected added

--- components/AddButton.tsx
+++ components/AddButton.tsx
@@ -1,18 +1,28 @@
 'use client'
 
-interface AddButtonProps{
+interface AddButtonProps {
+    variant: "primary" | "secondary"
     text: string
-    onClick?: ()=>void
+    onClick?: () => void
+    isSelected?: boolean
 }
 
 export default function AddButton(props: AddButtonProps) {
+    const { variant, text, onClick, isSelected } = props;
+
+    const variantClass = {
+        "primary": `block rounded-md px-3 py-2 text-center text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+            ${isSelected ? "bg-black text-white" : "bg-gray-200 text-black hover:bg-gray-300"}`,
+        "secondary": "block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    };
+
     return (
         <button
             type="button"
-            className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={props.onClick}
+            className={variantClass[variant]} 
+            onClick={onClick}
         >
-            {props.text}
+            {text}
         </button>
-    )
+    );
 }
