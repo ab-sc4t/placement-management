06a118151fe9b6dab4c94319efa8f1270b7811e4
ab-sc4t <ab.ayush2612@gmail.com>
2025-02-06 18:02:58 +0530

    job page updated new UI

--- app/jobdetails/page.tsx
+++ app/jobdetails/page.tsx
@@ -1,10 +1,13 @@
+import Button from "@/components/Button";
 import Header from "@/components/Header"
 import SalaryTable from "@/components/SalaryTable"
+import { TopRightArrow } from "@/icons/TopRightArrow";
 import { authOptions } from "@/lib/auth"
 import { getServerSession } from "next-auth"
 
 async function getSalaryData() {
     try {
+        console.log("waiting");
         const response = await fetch('http://localhost:3000/api/jobs')
         const data = await response.json()
         return data
@@ -18,18 +21,17 @@ export default async function SalariesPage() {
     const salaryData = await getSalaryData()
     const session = await getServerSession(authOptions)
     console.log(session);
-    
     return (
         <div className="min-h-screen bg-gray-100">
             <Header session={session}/>
             <div className="py-8">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="px-16 sm:px-6 lg:px-16">
                     <div className="sm:flex sm:items-center">
                         <div className="sm:flex-auto">
-                            <h1 className="text-2xl font-semibold leading-6 text-gray-900">Company Salaries</h1>
-                            <p className="mt-2 text-sm text-gray-700">
-                                A list of all companies and their CTC offerings.
-                            </p>
+                            <h1 className="text-2xl font-semibold leading-6 text-gray-900">Company Listing</h1>
+                        </div>
+                        <div>
+                            <Button text="My Job Board" href="/dashboard" endingLogo={<TopRightArrow size="24"/>}/>
                         </div>
                     </div>
                     <SalaryTable salaryData={salaryData} />
